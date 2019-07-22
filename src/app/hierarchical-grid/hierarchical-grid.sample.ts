8149152f76b458094459fe2afe983394f1ad3230
VBogomolov <vbogomolov@infragistics.com>
2019-07-22 12:00:25 +0300

    feat(paginator): samples updates #5118

--- src/app/hierarchical-grid/hierarchical-grid.sample.ts
+++ src/app/hierarchical-grid/hierarchical-grid.sample.ts
@@ -21,7 +21,12 @@ export class HierarchicalGridSampleComponent {
     @ViewChild('hGrid', { static: true })
     hGrid: IgxHierarchicalGridComponent;
 
-
+    public showPager = true;
+    public showDropDown = true;
+    public disablePager = false;
+    public disableDropDown = false;
+    public selectOptions = [7, 19, 25, 100];
+    public perPage = 15;
     constructor() {
         // this.localData.push({ ID: -1, Name: ''});
         // for (let i = 0; i < 10000; i++) {
@@ -42,17 +47,22 @@ export class HierarchicalGridSampleComponent {
         this.localData = this.generateDataUneven(100, 3);
     }
 
+    public handleChange(value: string) {
+        console.log('hi');
+        this.perPage = Number(value);
+    }
     generateData(count: number, level: number) {
         const prods = [];
         const currLevel = level;
         let children;
         for (let i = 0; i < count; i++) {
-           if (level > 0 ) {
-               children = this.generateData(count / 2 , currLevel - 1);
-           }
-           prods.push({
-            ID: i, ChildLevels: currLevel,  ProductName: 'Product: A' + i, 'Col1': i,
-            'Col2': i, 'Col3': i, childData: children, childData2: children });
+            if (level > 0) {
+                children = this.generateData(count / 2, currLevel - 1);
+            }
+            prods.push({
+                ID: i, ChildLevels: currLevel, ProductName: 'Product: A' + i, 'Col1': i,
+                'Col2': i, 'Col3': i, childData: children, childData2: children
+            });
         }
         return prods;
     }
@@ -63,19 +73,20 @@ export class HierarchicalGridSampleComponent {
         let children;
         for (let i = 0; i < count; i++) {
             const rowID = parendID ? parendID + i : i.toString();
-            if (level > 0 ) {
-               // Have child grids for row with even id less rows by not multiplying by 2
-               children = this.generateDataUneven((i % 2 + 1) * Math.round(count / 3) , currLevel - 1, rowID);
+            if (level > 0) {
+                // Have child grids for row with even id less rows by not multiplying by 2
+                children = this.generateDataUneven((i % 2 + 1) * Math.round(count / 3), currLevel - 1, rowID);
             }
             prods.push({
-                ID: rowID, ChildLevels: currLevel,  ProductName: 'Product: A' + i, 'Col1': i,
-                'Col2': i, 'Col3': i, childData: children, childData2: children });
+                ID: rowID, ChildLevels: currLevel, ProductName: 'Product: A' + i, 'Col1': i,
+                'Col2': i, 'Col3': i, childData: children, childData2: children
+            });
         }
         return prods;
     }
 
     setterChange() {
-       this.layout1.rowSelectable = !this.layout1.rowSelectable;
+        this.layout1.rowSelectable = !this.layout1.rowSelectable;
     }
 
     setterBindingChange() {
